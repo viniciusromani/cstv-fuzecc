b792fa99f6d02a49d65273e279412aa508643fc4
Vinicius Romani <vn.romani@gmail.com>
2024-09-02 10:33:49 -0300

    initial commit

---
+++ cstv-fuzecc/cstv_fuzeccApp.swift
@@ -0,0 +1,17 @@
+//
+//  cstv_fuzeccApp.swift
+//  cstv-fuzecc
+//
+//  Created by Vinicius Romani on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct cstv_fuzeccApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
